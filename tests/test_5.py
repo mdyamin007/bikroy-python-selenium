fa6b51458719bd0b2c9b2588fd1652d2dcfd0e25
sh <sh.qups@gmail.com>
2021-06-20 19:38:24 +0600

    jenkins file added and readme modified

--- tests/test_5.py
+++ tests/test_5.py
@@ -4,6 +4,7 @@ from pages.home_page import HomePage
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 from pages.settings_page import SettingsPage
+import time
 
 @allure.severity(allure.severity_level.NORMAL)
 class Test_5(BaseTest):
@@ -19,3 +20,14 @@ class Test_5(BaseTest):
         dashboard_page.click_settings()
         settings_page = SettingsPage(driver=self.driver)
         settings_page.change_password("yamin787898", "Y@min787898")
+
+        time.sleep(5)
+        settings_page.open("/")
+        time.sleep(5)
+        home_page.click_my_account()
+        self.driver.implicitly_wait(20)
+        dashboard_page = DashboardPage(driver=self.driver)
+        dashboard_page.click_settings()
+        self.driver.implicitly_wait(20)
+        settings_page = SettingsPage(driver=self.driver)
+        settings_page.change_password("Y@min787898", "yamin787898")
